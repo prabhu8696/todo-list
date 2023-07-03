f61a43cca221921fec24ef8cd91e937d31560a21
prabhu8696 <pthuletiya@gmail.com>
2023-07-03 17:45:01 +0530

    push all project

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from main.models import Task
 
-# Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('task','is_compeleted','updated')
+    search_fields = ('task',)
+
+admin.site.register(Task, TaskAdmin)
--- main/views.py
+++ main/views.py
@@ -1,4 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
+
+from main.models import Task
+
 
 def home(request):
-    return render(request,'main/home.html')
+    tasks = Task.objects.filter(is_compeleted=False).order_by('updated')
+    compeleted_task = Task.objects.filter(is_compeleted=True).order_by('updated')
+    context = {
+        'tasks' : tasks,
+        'compeleted_task' : compeleted_task,
+    }
+    return render(request,'main/home.html',context)
+
+def add_task(request):
+    task = request.POST['task']
+    Task.objects.create(task=task)
+    return redirect('home')
+
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_compeleted = True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_compeleted = False
+    task.save()
+    return redirect('home')
+
+def edit_task(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context = {
+            "get_task":get_task,
+        }
+        return render(request,'main/edit.html', context)
+    
+def delete_task(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    get_task.delete()
+    return redirect('home')
